820dfad8ffd908566c7688ce7ae42c9931a93afb
Putri-feby <putriifebyhandayani@gmail.com>
2025-06-19 13:40:43 +0700

    terima

--- iaekel2/iae/Kasir-Restoran-laravel-8-main (1)/Kasir-Restoran-laravel-8-main/public/polished/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	var current = window.location.href;
-	$('ul li a').each(function(){
-		var $this = $(this);
-		if ($this.attr('href') == current ) {
-			$this.parents('li').addClass('active');
-		}
-	});
-});

60b24293a887399706a65a1707c51a2aa5e00a32
sajidahfitriyani <sajidahfitriyani5@gmail.com>
2025-06-20 14:52:58 +0700

    menambahkan asset gambar

--- app/Http/Controllers/Frontend/MenuController.php
+++ app/Http/Controllers/Frontend/MenuController.php
@@ -17,29 +17,28 @@ class MenuController extends Controller
     public function cart()
     {
         $cartItems = session('cart', []);
-        // Debug: cek isi session
         //dd($cartItems);
         return view('menus.cart', compact('cartItems'));
     }
     
-public function addToCart($id)
-{
-    $menu = Menu::findOrFail($id);
-    $cart = session('cart', []);
+    public function addToCart($id)
+    {
+        $menu = Menu::findOrFail($id);
+        $cart = session('cart', []);
 
-    if(isset($cart[$id])) {
-        $cart[$id]['quantity']++;
-    } else {
-        $cart[$id] = [
-            'id' => $menu->id,
-            'name' => $menu->name,
-            'price' => $menu->price,
-            'quantity' => 1
-        ];
+        if(isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                'id' => $menu->id,
+                'name' => $menu->name,
+                'price' => $menu->price,
+                'quantity' => 1
+            ];
+        }
+
+        session(['cart' => $cart]);
+        return redirect()->route('cart.index')->with('success', 'Menu berhasil ditambahkan ke keranjang');
     }
-
-    session(['cart' => $cart]);
-    return redirect()->route('cart.index')->with('success', 'Menu berhasil ditambahkan ke keranjang');
-}
 
 }
